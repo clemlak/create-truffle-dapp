0900fc44a73531a74c125cef546574ceb4ca6ed3
Clement Lakhal <clementlakhal@gmail.com>
2018-08-13 20:42:17 +0200

    Minor update

--- templates/project/test/Base.test.js
+++ templates/project/test/Base.test.js
@@ -6,14 +6,14 @@ const _project_placeholder_ = artifacts.require('_project_placeholder_');
 let instance;
 
 contract('_project_placeholder_', (accounts) => {
-  it('Should deploy an instance of the _contract_placeholder_ contract', () => _project_placeholder_.deployed().then((contractInstance) => {
+  it('Should deploy an instance of the _project_placeholder_ contract', () => _project_placeholder_.deployed().then((contractInstance) => {
     instance = contractInstance;
   }));
 
   it('Should set the number', () => instance.setNumber(2).then(() => {
   }));
 
-  it('Should get the numbre', () => instance.getNumber().then((number) => {
+  it('Should get the number', () => instance.getNumber().then((number) => {
     assert.equal(number.toNumber(), 2, 'Number is wrong!');
   }));
 });
