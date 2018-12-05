2c7480f379257ce8a185ab06201a6e923d86db89
Clement Lakhal <clementlakhal@gmail.com>
2018-12-05 09:26:59 +0100

    Fixed syntax

--- templates/project/test/Base.test.js
+++ templates/project/test/Base.test.js
@@ -6,9 +6,10 @@ const _project_placeholder_ = artifacts.require('_project_placeholder_');
 let instance;
 
 contract('_project_placeholder_', (accounts) => {
-  it('Should deploy an instance of the _project_placeholder_ contract', () => _project_placeholder_.deployed().then((contractInstance) => {
-    instance = contractInstance;
-  }));
+  it('Should deploy an instance of the _project_placeholder_ contract', () => _project_placeholder_.deployed()
+    .then((contractInstance) => {
+      instance = contractInstance;
+    }));
 
   it('Should set the number', () => instance.setNumber(2));
 
